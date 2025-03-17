d6e0e6814aa2f5478ad12e73096d0e7ebc5fe7be
Fernanda Morales <ferchis.and.bees@gmail.com>
2025-03-17 12:59:47 -0600

    el xml ya se guarda

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+const cors = require("cors");
+
 const app = express();
-const bodyParser=require('body-parser');
-const multipart=require('connect-multiparty');
-const PORT = 3000;
 
-const multiPartMiddleware=multipart({
-    uploadDir:'./subidas'
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-extented:true
-}));
-//endpoint para subir archivos
 
-app.post('./api/subir', multiPartMiddleware,(req,res)=>{
-res.json({
-    'message':'Fichero subido correctamente'
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Configurar `multer` para guardar imágenes en `public/assets`
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, "public", "assets")); // Guarda en `public/assets`
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname); // Usa el nombre original
+    }
+  });
+  const upload = multer({ storage });
+
+
+app.post("/guardar-xml", (req, res) => {
+  const xmlData = req.body.xml;
+  const filePath = path.resolve("public/assets/productos.xml");
+  fs.writeFile(filePath, xmlData, (err) => {
+    if (err) {
+      console.error("Error al guardar XML:", err);
+      return res.status(500).send("Error al guardar el archivo");
+    }
+    res.send("Archivo guardado en: " + filePath);
+  });
 });
 
-});
-
-app.get('/',(req,res)=>{
-res.send('Hola mundo');
-});
-
-
-app.listen(PORT,()=>console.log(`App running on port: ${PORT}`))
+app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
